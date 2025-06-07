05a8bd55d869f42ddc16e1214c307f5f2f147b61
khachungdev <email_cua_khachungdev@gmail.com>
2025-06-07 11:45:28 +0700

    New

--- be/routes/api.php
+++ be/routes/api.php
@@ -3,12 +3,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BannerController;
 use App\Http\Controllers\CategoryController;
 
-
-<<<<<<< HEAD
-Route::resource('productsadmin', CrudProductController::class);
-=======
 Route::get('/categories', [CategoryController::class, 'index']);
 Route::post('/banners', [BannerController::class, 'store']);
-
->>>>>>> origin/dev
 
