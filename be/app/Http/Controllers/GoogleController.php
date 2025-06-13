4c57271f7ea9a45f387555694a227e1c7633fc33
khachungdev <email_cua_khachungdev@gmail.com>
2025-06-13 23:35:25 +0700

    WIP: temporary commit before pull

---
+++ be/app/Http/Controllers/GoogleController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Laravel\Socialite\Facades\Socialite;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+class GoogleController extends Controller
+{
+    public function redirectToGoogle()
+    {
+        return Socialite::driver('google')->redirect();
+    }
+
+    public function handleGoogleCallback()
+    {
+        try {
+            $googleUser = Socialite::driver('google')->stateless()->user();
+
+            $user = User::firstOrCreate(
+                ['email' => $googleUser->getEmail()],
+                ['name' => $googleUser->getName()]
+            );
+
+            Auth::login($user);
+
+            return redirect('/home'); 
+
+        } catch (\Exception $e) {
+            return redirect('/login')->with('error', 'Đăng nhập Google thất bại!');
+        }
+    }
+}
