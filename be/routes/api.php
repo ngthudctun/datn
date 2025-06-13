c5bdeba3c8def8cc39f2a9d2b7255e88d11e5d64
trungmaybe <nguyentuantrung06032005@gmail.com>
2025-06-13 12:58:05 +0700

    done đăng nhập gg và thêm chi tiết đăng kí có 1 trang home trong views

--- be/routes/api.php
+++ be/routes/api.php
@@ -15,29 +15,30 @@ use App\Http\Controllers\ApiWishlistController;
 use App\Http\Controllers\API\ProductController as APIProductController;
 use Illuminate\Support\Facades\Request;
 
+/* api của trung */
+Route::get('/products/latest', [ProductController::class, 'latestFive']);
+Route::apiResource('products', ProductController::class);
+
+/*API cua trung */
 Route::apiResource('categories', CategoryController::class);
 Route::apiResource('banners', BannerController::class);
 Route::apiResource('logins', LoginController::class);
 
-Route::post('/register', [AuthController::class, 'createUser']);
-Route::post('/login', [AuthController::class, 'login'])->name('login');
-
+/*API cua trung */
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/users', [AuthController::class, 'index']);
-    Route::post('/logout', [AuthController::class, 'logout']);
-});
-
-Route::get('/forgot-password', [ForgotPasswordController::class, 'sendResetLink']);
-Route::get('/reset-password', [ForgotPasswordController::class, 'resetPassword']);
-
-Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/logout', [AuthController::class, 'logout']);
+    Route::get('/register', [AuthController::class, 'createUser']);
+    Route::post('/login', [AuthController::class, 'login'])->name('login');
     Route::apiResource('/cart/toggle', CartController::class);
-});
-
-Route::middleware('auth:sanctum')->group(function () {
     Route::get('/wishlist/toggle', [ApiWishlistController::class, 'toggle']);
     Route::get('/wishlist', [ApiWishlistController::class, 'index']);
 });
+
+/*API cua trung */
+Route::get('/forgot-password', [ForgotPasswordController::class, 'sendResetLink']);
+Route::get('/reset-password', [ForgotPasswordController::class, 'resetPassword']);
+
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -52,8 +53,7 @@ Route::delete('/products/{product}', [APIProductController::class, 'destroy']);
 
 
 /* api của truong */
-Route::resource('seller-category', SellerCateController::class);
+Route::apiResource('seller-category', SellerCateController::class);
 Route::get('seller-image-gate', [ImageSelected::class, 'index']);
 
-/* api của truong */
-Route::get('/products/latest', [ProductController::class, 'latestFive']);
+
--- be/routes/web.php
+++ be/routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\GoogleController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/auth/google', [GoogleController::class, 'redirectToGoogle']);
+Route::get('/auth/google/callback', [GoogleController::class, 'handleGoogleCallback']);
